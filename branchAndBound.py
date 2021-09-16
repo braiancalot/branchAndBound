2aaa0630c11eeca731118385141dee3e2e863518
Braian dos Santos Calot <calot665@hotmail.com>
2021-09-15 21:51:10 -0300

    Update branchAndBound.py

--- branchAndBound.py
+++ branchAndBound.py
@@ -1,15 +1,16 @@
-class No:
+
+class No: #criação da classe que será usada para representar cada ponto(x,y) na matriz, e seu custo
     def __init__(self, posicao, pai):
         self.posicao = posicao
         self.pai = pai
         self.g = 0
         self.h = 0
         self.f = 0
-    
-def getF(no):
+
+def getF(no): # função para permitir a ordenação da lista aberta de acordo com os custo de cada no
     return no.f
 
-def bnb(mat, start, goal):
+def bnb(mat, start, goal): # implementação do branch-and-bound
     numCol = len(mat[0])
     numLin = len(mat)
     listaAberta = []
@@ -65,7 +66,7 @@ def bnb(mat, start, goal):
 
     return None
 
-def calcularDistancia(noOrigem, noDestino):
+def calcularDistancia(noOrigem, noDestino): # calcular a distancia entre duas celulas, levando em conta a possibilidade da movimentação na diagonal
     distX = abs(noOrigem.posicao[0] - noDestino.posicao[0])
     distY = abs(noOrigem.posicao[1] - noDestino.posicao[1])
 
@@ -74,12 +75,14 @@ def calcularDistancia(noOrigem, noDestino):
     else:
         return distY
 
-def addListaAberta(listaAberta, noProximo):
+def addListaAberta(listaAberta, noProximo): # verificar se o no atual já se encontra na lista aberta, e caso contrario, adiciona-lo 
     for no in listaAberta:
-        if(noProximo.posicao == no.posicao and noProximo.f >= noProximo.f):
+        if(noProximo.posicao == no.posicao):
             return False
     return True
 
+
+# definição das matrizes pré-determinadas
 mat1 = [[1, 0], 
         [0, 1]]
 
@@ -131,14 +134,9 @@ mat8 =  [[1, 0, 0, 4, 0, 1],
          [0, 0, 1, 0, 1, -1],
          [0, 1, 0, 0, 1, 1],
          [5, 0, 0, 0, 1, -1]]
+        
 
-mat9 = [[0, 1, 0],
-        [1, 0, 1],
-        [10, 0, 12],
-        [10, 0, 1],
-        [0, 1, 0]]         
-
-def imprimirMatriz(mat):
+def imprimirMatriz(mat): # imprimir matriz formatada
     lin = len(mat)
     col = len(mat[0])
 
@@ -147,7 +145,7 @@ def imprimirMatriz(mat):
             print(f'{num:>3}', end=" ")
         print()
 
-def verificarEntradas(mat, start, goal):
+def verificarEntradas(mat, start, goal): # verificar se as coordenadas do START e GOAL são validas
     lin = len(mat)
     col = len(mat[0])
 
@@ -157,10 +155,24 @@ def verificarEntradas(mat, start, goal):
     if((start[0] >= lin or start[1] >= col) or (goal[0] >= lin or goal[1] >= col)):
         return False
 
-    if(mat[start[0]][start[1]] == 0 or mat[start[0]][start[1]] == 0):
+    if(mat[start[0]][start[1]] == 0 or mat[goal[0]][goal[1]] == 0):
         return False
 
     return True
+
+def verificarInput(input): # verificar se o input do usuario é valido
+    try:
+        int(input)
+        return True
+    except ValueError:
+        return False
+
+
+
+
+################################################################################################################
+#-------------------------------------------------MENU---------------------------------------------------------#
+################################################################################################################
 
 op = None
 while op != 0:
@@ -170,7 +182,11 @@ while op != 0:
     print("2. Criar Matriz")
     print("0. Sair")
     print("--------------------")
-    op = int(input("Digite a opção: "))
+    op = input("Digite a opção: ")
+    while(verificarInput(op) == False):
+        op = input("Opção incorreta, digite novamente: ")
+    
+    op = int(op)
 
     if op == 1:        
         print("\nMatriz 1:\n")
@@ -190,147 +206,221 @@ while op != 0:
         print("\nMatriz 8:\n")
         imprimirMatriz(mat8)
 
-        op2 = int(input("\nDigite o número da matriz que deseja trabalhar: "))
-        print("Digite as coordenadas do START e GOAL, lembre-se: Só é possível começar e terminar em uma célula cujo o valor é igual ou maior que 1")
-        startX = int(input("\nDigite a linha do START: "))
-        startY = int(input("Digite a coluna do START: "))
-        goalX = int(input("Digite a linha do GOAL: "))
-        goalY = int(input("Digite a coluna do GOAL: "))
         
-        start = (startX, startY)
-        goal = (goalX, goalY)   
+        op2 = input("\nDigite o número da matriz que deseja trabalhar: ")
+        while(verificarInput(op2) == False or int(op2) < 1 or int(op2) > 8):
+            print("Entrada inválida, digite novamente.")
+            op2 = input("Digite o número da matriz que deseja trabalhar: ")
+        op2 = int(op2)    
 
-        if op2 == 1:
-            if(verificarEntradas(mat1, start, goal)):
-                path = bnb(mat1, start, goal)
-                print("\nMelhor caminho encontrado: \n")
-                for i in range(len(path)-1, -1, -1):
-                    print(path[i], end='\t')
+        sair = True
+        while(sair):
+            print("Digite as coordenadas do START e GOAL, lembre-se: Só é possível começar e terminar em uma célula cujo o valor é igual ou maior que 1")
+            
+            startX = input("\nDigite a linha do START: ")
+            while(verificarInput(startX) == False):
+                print("Entrada inválida")
+                startX = input("Digite a linha do START: ")
+            startX = int(startX)
 
-                print()
-            else:
-                print("Informações inválidas! Tente novamente.")  
+            startY = input("Digite a coluna do START: ")
+            while(verificarInput(startY) == False):
+                print("Entrada inválida")
+                startY = input("Digite a coluna do START: ")
+            startY = int(startY)
 
-        elif op2 == 2:
-            if(verificarEntradas(mat2, start, goal)):
-                path = bnb(mat2, start, goal)
-                print("\nMelhor caminho encontrado: \n")
-                for i in range(len(path)-1, -1, -1):
-                    print(path[i], end='\t')
+            goalX = input("Digite a linha do GOAL: ")
+            while(verificarInput(goalX) == False):
+                print("Entrada inválida")
+                goalX = input("Digite a linha do GOAL: ")
+            goalX = int(goalX)
 
-                print()
+            goalY = input("Digite a coluna do GOAL: ")
+            while(verificarInput(goalY) == False):
+                print("Entrada inválida")
+                goalY = input("Digite a coluna do GOAL: ")
+            goalY = int(goalY)
+            
+            start = (startX, startY)
+            goal = (goalX, goalY)   
+        
+            if op2 == 1:
+                if(verificarEntradas(mat1, start, goal)):
+                    path = bnb(mat1, start, goal)
+                    print("\nMelhor caminho encontrado: \n")
+                    for i in range(len(path)-1, -1, -1):
+                        print(path[i], end='\t')
+
+                    print()
+                else:
+                    print("Informações inválidas! Tente novamente.")  
+
+            elif op2 == 2:
+                if(verificarEntradas(mat2, start, goal)):
+                    path = bnb(mat2, start, goal)
+                    print("\nMelhor caminho encontrado: \n")
+                    for i in range(len(path)-1, -1, -1):
+                        print(path[i], end='\t')
+
+                    print()
+                else:
+                    print("Informações inválidas! Tente novamente.")
+
+            elif op2 == 3:
+                if(verificarEntradas(mat3, start, goal)):
+                    path = bnb(mat3, start, goal)
+                    print("\nMelhor caminho encontrado: \n")
+                    for i in range(len(path)-1, -1, -1):
+                        print(path[i], end='\t')
+
+                    print()
+                else:
+                    print("Informações inválidas! Tente novamente.")  
+
+            elif op2 == 4:
+                if(verificarEntradas(mat4, start, goal)):
+                    path = bnb(mat4, start, goal)
+                    print("\nMelhor caminho encontrado: \n")
+                    for i in range(len(path)-1, -1, -1):
+                        print(path[i], end='\t')
+
+                    print()
+                else:
+                    print("Informações inválidas! Tente novamente.")
+
+            elif op2 == 5:
+                if(verificarEntradas(mat5, start, goal)):
+                    path = bnb(mat5, start, goal)
+                    print("\nMelhor caminho encontrado: \n")
+                    for i in range(len(path)-1, -1, -1):
+                        print(path[i], end='\t')
+
+                    print()
+                else:
+                    print("Informações inválidas! Tente novamente.")
+
+            elif op2 == 6:
+                if(verificarEntradas(mat6, start, goal)):
+                    path = bnb(mat6, start, goal)
+                    print("\nMelhor caminho encontrado: \n")
+                    for i in range(len(path)-1, -1, -1):
+                        print(path[i], end='\t')
+
+                    print()
+                else:
+                    print("Informações inválidas! Tente novamente.")
+
+            elif op2 == 7:
+                if(verificarEntradas(mat7, start, goal)):
+                    path = bnb(mat7, start, goal)
+                    print("\nMelhor caminho encontrado: \n")
+                    for i in range(len(path)-1, -1, -1):
+                        print(path[i], end='\t')
+
+                    print()
+                else:
+                    print("Informações inválidas! Tente novamente.")
+
+            elif op2 == 8:
+                if(verificarEntradas(mat8, start, goal)):
+                    path = bnb(mat8, start, goal)
+                    print("\nMelhor caminho encontrado: \n")
+                    for i in range(len(path)-1, -1, -1):
+                        print(path[i], end='\t')
+
+                    print()
+                else:
+                    print("Informações inválidas! Tente novamente.")
+
             else:
                 print("Informações inválidas! Tente novamente.")
 
-        elif op2 == 3:
-            if(verificarEntradas(mat3, start, goal)):
-                path = bnb(mat3, start, goal)
-                print("\nMelhor caminho encontrado: \n")
-                for i in range(len(path)-1, -1, -1):
-                    print(path[i], end='\t')
-
-                print()
-            else:
-                print("Informações inválidas! Tente novamente.")  
-
-        elif op2 == 4:
-            if(verificarEntradas(mat4, start, goal)):
-                path = bnb(mat4, start, goal)
-                print("\nMelhor caminho encontrado: \n")
-                for i in range(len(path)-1, -1, -1):
-                    print(path[i], end='\t')
-
-                print()
-            else:
-                print("Informações inválidas! Tente novamente.")
-
-        elif op2 == 5:
-            if(verificarEntradas(mat5, start, goal)):
-                path = bnb(mat5, start, goal)
-                print("\nMelhor caminho encontrado: \n")
-                for i in range(len(path)-1, -1, -1):
-                    print(path[i], end='\t')
-
-                print()
-            else:
-                print("Informações inválidas! Tente novamente.")
-
-        elif op2 == 6:
-            if(verificarEntradas(mat6, start, goal)):
-                path = bnb(mat6, start, goal)
-                print("\nMelhor caminho encontrado: \n")
-                for i in range(len(path)-1, -1, -1):
-                    print(path[i], end='\t')
-
-                print()
-            else:
-                print("Informações inválidas! Tente novamente.")
-
-        elif op2 == 7:
-            if(verificarEntradas(mat7, start, goal)):
-                path = bnb(mat7, start, goal)
-                print("\nMelhor caminho encontrado: \n")
-                for i in range(len(path)-1, -1, -1):
-                    print(path[i], end='\t')
-
-                print()
-            else:
-                print("Informações inválidas! Tente novamente.")
-
-        elif op2 == 8:
-            if(verificarEntradas(mat8, start, goal)):
-                path = bnb(mat8, start, goal)
-                print("\nMelhor caminho encontrado: \n")
-                for i in range(len(path)-1, -1, -1):
-                    print(path[i], end='\t')
-
-                print()
-            else:
-                print("Informações inválidas! Tente novamente.")
-
-        else:
-            print("Informações inválidas! Tente novamente.")
+            escolha = input("\nDeseja tentar novamente com a mesma matrz? (S/N): ")
+            if(escolha == "N" or escolha == "n"):
+                sair = False
+                break
     
-    if op == 2:
-        qtLin = int(input("Digite a quantidade de linhas: "))
-        qtCol = int(input("Digite a quantidade de colunas: "))  
+    elif op == 2:
+        qtLin = input("Digite a quantidade de linhas: ")
+        while(verificarInput(qtLin) == False):
+            print("Entrada inválida, digite novamente.")
+            qtLin = input("Digite a quantidade de linhas: ")
+        qtLin = int(qtLin)
+        
+        qtCol = input("Digite a quantidade de colunas: ")
+        while(verificarInput(qtCol) == False):
+            print("Entrada inválida, digite novamente: ")
+            qtCol = input("Digite a quantidade de colunas: ")
+        qtCol = int(qtCol)  
+
         print("\nPreencha a matriz: (Lembre-se: 0 ou -1 = não da pra passar por essa célula, 1 ou maior = é possível passar e o custo para passar por essa célula\n")
       
         matCriada = []
         for i in range(qtLin):
             linha = []
             for j in range(qtCol):
-                num = int(input("Digite o elemento[" + str(i) + "][" + str(j) + "]: "))
+                num = input("Digite o elemento[" + str(i) + "][" + str(j) + "]: ")
+                while(verificarInput(num) == False):
+                    print("Entrada inválida, digite novamente. ")
+                    num = input("Digite o elemento[" + str(i) + "][" + str(j) + "]: ")
+                num = int(num)
                 linha.append(num)
             matCriada.append(linha)
 
         print("\nMatriz criada:\n ")
         imprimirMatriz(matCriada)  
 
-        print("\nDigite as coordenadas do START e GOAL, lembre-se: Só é possível começar e terminar em uma célula cujo o valor é igual ou maior que 1")
-        startX = int(input("\nDigite a linha do START: "))
-        startY = int(input("Digite a coluna do START: "))
-        goalX = int(input("Digite a linha do GOAL: "))
-        goalY = int(input("Digite a coluna do GOAL: "))
-        
-        start = (startX, startY)
-        goal = (goalX, goalY)
+        sair = True
+        while(sair):
+            print("\nDigite as coordenadas do START e GOAL, lembre-se: Só é possível começar e terminar em uma célula cujo o valor é igual ou maior que 1")
+            
+            startX = input("\nDigite a linha do START: ")
+            while(verificarInput(startX) == False):
+                print("Opção Inválida")
+                startX = input("Digite a linha do START: ")
+            startX = int(startX)
 
-        if(verificarEntradas(matCriada, start, goal)):
-            path = bnb(matCriada, start, goal)
-            print("\nMelhor caminho encontrado: \n")
-            for i in range(len(path)-1, -1, -1):
-                print(path[i], end='\t')
+            startY = input("Digite a coluna do START: ")
+            while(verificarInput(startY) == False):
+                print("Opção Inválida")
+                startY = input("Digite a coluna do START: ")
+            startY = int(startY)
 
-            print()
-        else:
-            print("Informações inválidas! Tente novamente.")
+            goalX = input("Digite a linha do GOAL: ")
+            while(verificarInput(goalX) == False):
+                print("Opção Inválida")
+                goalX = input("Digite a linha do GOAL: ")
+            goalX = int(goalX)
 
-    elif op == 2:
-        print("Opção 2")     
+            goalY = input("Digite a coluna do GOAL: ")
+            while(verificarInput(goalY) == False):
+                print("Opção Inválida")
+                goalY = input("Digite a coluna do GOAL: ")
+            goalY = int(goalY) 
+            
+            start = (startX, startY)
+            goal = (goalX, goalY)
+
+            if(verificarEntradas(matCriada, start, goal)):
+                path = bnb(matCriada, start, goal)
+                print("\nMelhor caminho encontrado: \n")
+                for i in range(len(path)-1, -1, -1):
+                    print(path[i], end='\t')
+
+                print()
+            else:
+                print("\nInformações inválidas! Tente novamente.")
+
+            
+            escolha = input("\nDeseja tentar novamente com a mesma matrz? (S/N): ")   
+            if(escolha == "N" or escolha == "n"):
+                sair = False    
     
     elif op == 0:
        print("Saindo...") 
-    else:
-        print("Opção inválida! Tente novamente.")       
+    
+    elif(op > 2 or sair != False):
+        print("\nOpção inválida! Tente novamente.")    
+
+
    
